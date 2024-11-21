60a85b43f4b00ac05270dea0e8945eb59685de53
MaiQuocHuy <huymq.21it@vku.udn.vn>
2024-11-21 12:21:34 +0700

    final

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -1,24 +1,25 @@
 // models/Todo.js
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/mysql.config');
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/mysql.config");
 
-class Todo extends Model { }
+class Todo extends Model {}
 
 Todo.init(
-    {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    { sequelize }
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  { sequelize }
 );
-sequelize.sync()
-    .then(() => console.log('Database connected'))
-    .catch(err => console.log('Error: ' + err));
+sequelize
+  .sync()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Error: " + err));
 
 module.exports = Todo;
